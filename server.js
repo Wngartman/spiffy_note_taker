a0eaac02c00cda17190e873c6da99d2064f47b51
William Gartman <wngartman@gmail.com>
2022-02-02 11:22:09 -0600

    more heroku troubleshooting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes')
-const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./develop/routes/htmlRoutes')
+const apiRoutes = require('./develop/routes/apiRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
